6a034323ab7661559b3a087448ec48ffb8ba8a00
solarspecter315 <ted563473@gmail.com>
2020-10-27 16:59:09 -0300

    working on new plugin- draw and mouse tracking

--- jspsych/plugins/jspsych-draw-and-mouse-tracking.js
+++ jspsych/plugins/jspsych-draw-and-mouse-tracking.js
@@ -1,15 +1,31 @@
-jsPsych.plugins['plugin-name'] = (function(){
+jsPsych.plugins['draw-and-mouse-tracking'] = (function(){
 
     var plugin = {};
   
     plugin.info = {
-      name: 'plugin-name',
+      name: 'draw-and-mouse-tracking',
       parameters: {
       }
     }
   
     plugin.trial = function(display_element, trial){
-      jsPsych.finishTrial();
+      //display_element is a HTML DOM element
+
+      //saving data
+      let trial_data = {
+        //parameter_name = 'parameter value';
+        words: trial.words
+      };
+
+      function show_stimulus(){
+        display_element.innerHTML = '<p>' + trial.words + '</p>';
+
+      };
+
+      show_stimulus();
+
+      // end trial
+      jsPsych.finishTrial(trial_data);
     }
   
     return plugin;
